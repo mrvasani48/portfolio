aab6e18b46dac910c4ae2494b007e9aeb4d61bac
mrvasani48 <vasanikishan48@gmail.com>
2023-05-17 19:33:41 +0530

    feat:added skills

--- src/components/pages/dashboards/resume/ProfessionalSkills.tsx
+++ src/components/pages/dashboards/resume/ProfessionalSkills.tsx
@@ -1,9 +1,88 @@
-import React from 'react'
+import React from "react";
 
 const ProfessionalSkills = () => {
   return (
-    <div>ProfessionalSkills</div>
-  )
-}
+    <div className="mt-10">
+      <div className="grid grid-cols-2 gap-10">
+        <div>
+          <div className="mt-3">
+            <div className="flex justify-between">
+              <p>Design</p>
+              <p>89%</p>
+            </div>
+            <p className="mt-2 w-full h-2 bg-red-500 rounded-md"></p>
+          </div>
+          <div className="mt-3">
+            <div className="flex justify-between">
+              <p>Design</p>
+              <p>89%</p>
+            </div>
+            <p className="mt-2 w-full h-2 bg-red-500 rounded-md"></p>
+          </div>
+          <div className="mt-3">
+            <div className="flex justify-between">
+              <p>Design</p>
+              <p>89%</p>
+            </div>
+            <p className="mt-2 w-full h-2 bg-red-500 rounded-md"></p>
+          </div>
+          <div className="mt-3">
+            <div className="flex justify-between">
+              <p>Design</p>
+              <p>89%</p>
+            </div>
+            <p className="mt-2 w-full h-2 bg-red-500 rounded-md"></p>
+          </div>
+          <div className="mt-3">
+            <div className="flex justify-between">
+              <p>Design</p>
+              <p>89%</p>
+            </div>
+            <p className="mt-2 w-full h-2 bg-red-500 rounded-md"></p>
+          </div>
+        </div>
 
-export default ProfessionalSkills
+        <div>
+          <div className="mt-3">
+            <div className="flex justify-between">
+              <p>Html</p>
+              <p>89%</p>
+            </div>
+            <p className="mt-2 w-full h-2 bg-red-500 rounded-md"></p>
+          </div>
+          <div className="mt-3">
+            <div className="flex justify-between">
+              <p>Css</p>
+              <p>89%</p>
+            </div>
+            <p className="mt-2 w-full h-2 bg-red-500 rounded-md"></p>
+          </div>
+          <div className="mt-3">
+            <div className="flex justify-between">
+              <p>Javascript</p>
+              <p>89%</p>
+            </div>
+            <p className="mt-2 w-full h-2 bg-red-500 rounded-md"></p>
+          </div>
+          <div className="mt-3">
+            <div className="flex justify-between">
+              <p>React</p>
+              <p>89%</p>
+            </div>
+            <p className="mt-2 w-full h-2 bg-red-500 rounded-md"></p>
+          </div>
+          <div className="mt-3">
+            <div className="flex justify-between">
+              <p>Web Development</p>
+              <p>89%</p>
+            </div>
+            <p className="mt-2 w-full h-2 bg-red-500 rounded-md"></p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default ProfessionalSkills;
