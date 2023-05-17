14ba2a596e34f1f98ed1a6deba88ab1c433d004f
mrvasani48 <vasanikishan48@gmail.com>
2023-05-17 19:09:11 +0530

    feat:added education

--- src/components/pages/dashboards/resume/Education.tsx
+++ src/components/pages/dashboards/resume/Education.tsx
@@ -7,8 +7,131 @@ const Education = () => {
         <div>
           <p className="py-2 text-red-500">2018-2022</p>
           <h2 className="text-5xl dark:text-white">Education Quality</h2>
-          <div className="mt-10 py-10 pl-10">
-            <div>
+          <div>
+            <div className=" py-10 pl-10">
+              <div className="p-7 bg-dark-700">
+                <div className="grid grid-cols-12">
+                  <div className="col-span-10">
+                    <h2 className="text-2xl py-4">
+                      {" "}
+                      Diploma in Web Development{" "}
+                    </h2>
+                    <p>BSE In CSE (2004 - 2008) </p>
+                  </div>
+                  <div className="flex justify-center items-center">
+                    <p className="p-2 bg-dark-700 rounded-md text-red-500">
+                      4.70/5
+                    </p>
+                  </div>
+                </div>
+                <p className="py-5">
+                  Contrary to popular belief. Ut tincidunt est ac dolor aliquam
+                  sodales. Phasellus sed mauris hendrerit, laoreet sem in,
+                  lobortis mauris hendrerit ante.
+                </p>
+              </div>
+            </div>
+            <div className=" py-10 pl-10 pt-0">
+              <div className="p-7 bg-dark-700">
+                <div className="grid grid-cols-12">
+                  <div className="col-span-10">
+                    <h2 className="text-2xl py-4">
+                      {" "}
+                      Diploma in Web Development{" "}
+                    </h2>
+                    <p>BSE In CSE (2004 - 2008) </p>
+                  </div>
+                  <div className="flex justify-center items-center">
+                    <p className="p-2 bg-dark-700 rounded-md text-red-500">
+                      4.70/5
+                    </p>
+                  </div>
+                </div>
+                <p className="py-5">
+                  Contrary to popular belief. Ut tincidunt est ac dolor aliquam
+                  sodales. Phasellus sed mauris hendrerit, laoreet sem in,
+                  lobortis mauris hendrerit ante.
+                </p>
+              </div>
+            </div>
+            <div className=" py-10 pl-10 pt-0">
+              <div className="p-7 bg-dark-700">
+                <div className="grid grid-cols-12">
+                  <div className="col-span-10">
+                    <h2 className="text-2xl py-4">
+                      {" "}
+                      Diploma in Web Development{" "}
+                    </h2>
+                    <p>BSE In CSE (2004 - 2008) </p>
+                  </div>
+                  <div className="flex justify-center items-center">
+                    <p className="p-2 bg-dark-700 rounded-md text-red-500">
+                      4.70/5
+                    </p>
+                  </div>
+                </div>
+                <p className="py-5">
+                  Contrary to popular belief. Ut tincidunt est ac dolor aliquam
+                  sodales. Phasellus sed mauris hendrerit, laoreet sem in,
+                  lobortis mauris hendrerit ante.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="">
+        <p className="py-2 text-red-500">2018-2022</p>
+        <h2 className="text-5xl dark:text-white">Job Experience</h2>
+        <div>
+          <div className=" py-10 pl-10">
+            <div className="p-7 bg-dark-700">
+              <div className="grid grid-cols-12">
+                <div className="col-span-10">
+                  <h2 className="text-2xl py-4">
+                    {" "}
+                    Diploma in Web Development{" "}
+                  </h2>
+                  <p>BSE In CSE (2004 - 2008) </p>
+                </div>
+                <div className="flex justify-center items-center">
+                  <p className="p-2 bg-dark-700 rounded-md text-red-500">
+                    4.70/5
+                  </p>
+                </div>
+              </div>
+              <p className="py-5">
+                Contrary to popular belief. Ut tincidunt est ac dolor aliquam
+                sodales. Phasellus sed mauris hendrerit, laoreet sem in,
+                lobortis mauris hendrerit ante.
+              </p>
+            </div>
+          </div>
+          <div className=" py-10 pl-10 pt-0">
+            <div className="p-7 bg-dark-700">
+              <div className="grid grid-cols-12">
+                <div className="col-span-10">
+                  <h2 className="text-2xl py-4">
+                    {" "}
+                    Diploma in Web Development{" "}
+                  </h2>
+                  <p>BSE In CSE (2004 - 2008) </p>
+                </div>
+                <div className="flex justify-center items-center">
+                  <p className="p-2 bg-dark-700 rounded-md text-red-500">
+                    4.70/5
+                  </p>
+                </div>
+              </div>
+              <p className="py-5">
+                Contrary to popular belief. Ut tincidunt est ac dolor aliquam
+                sodales. Phasellus sed mauris hendrerit, laoreet sem in,
+                lobortis mauris hendrerit ante.
+              </p>
+            </div>
+          </div>
+          <div className=" py-10 pl-10 pt-0">
+            <div className="p-7 bg-dark-700">
               <div className="grid grid-cols-12">
                 <div className="col-span-10">
                   <h2 className="text-2xl py-4">
@@ -31,10 +154,6 @@ const Education = () => {
             </div>
           </div>
         </div>
-      </div>
-      <div className="">
-        <p className="py-2 text-red-500">2018-2022</p>
-        <h2 className="text-5xl dark:text-white">Job Experience</h2>
       </div>
     </div>
   );
